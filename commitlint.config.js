f264924fdda3f2cf0ba7f1f61395b745330df7e6
Alex Csiszar <csiszaralex@gmail.com>
2026-03-26 22:30:57 +0100

    fix(repo): update project list and pre-version command in nx.json

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,11 @@
-module.exports = { extends: ['@commitlint/config-conventional'] };
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'scope-enum': [
+      2,
+      'always',
+      ['api', 'worker', 'web', 'repo', 'ci', 'release'],
+    ],
+    'scope-empty': [2, 'never'],
+  },
+};
